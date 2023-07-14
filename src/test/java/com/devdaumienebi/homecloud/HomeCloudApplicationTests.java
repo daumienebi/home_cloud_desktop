7d8b414858fb521b7de679dd6ab324d8b779c715
daumienebi <ddsakpa@gmail.com>
2023-07-14 02:29:44 +0200

    switching to a full springboot project

---
+++ src/test/java/com/devdaumienebi/homecloud/HomeCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdaumienebi.homecloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
